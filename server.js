9be6d7a97c548c3101a73d4716b3373ee2c67865
Lucas Chartier <lucaspchartier@gmail.com>
2020-10-08 21:30:29 -0400

    Update response headers for function for CORS in server.js

--- server.js
+++ server.js
@@ -60,11 +60,8 @@ app.use((req, res, next) => {
   }
 
   res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
 
   next()
 })
