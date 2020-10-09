14e77f908f832877a374eeced26ae5e8c8f5c1a8
Lucas Chartier <lucaspchartier@gmail.com>
2020-10-08 20:53:57 -0400

    Reformat CORS function in server.js

--- server.js
+++ server.js
@@ -60,7 +60,11 @@ app.use((req, res, next) => {
   }
 
   res.header('Access-Control-Allow-Origin', 'http://localhost:7165')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
 
   next()
 })
