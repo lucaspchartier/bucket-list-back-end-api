469f48f3823478f2943f183541f844fd3af16e08
lucaspchartier <lucaspchartier@gmail.com>
2020-11-11 17:58:19 -0500

    Add '*' to header within app.use() function

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ const app = express()
 
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-// app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
-app.options('*', cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
+// app.options('*', cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
 
 // define port for API to run on
 const port = process.env.PORT || 4741
@@ -53,7 +53,7 @@ const port = process.env.PORT || 4741
 // of `Authorization: Token token=<token>` OR the Express convention of
 // `Authorization: Bearer <token>`
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_ORIGIN)
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   if (req.headers.authorization) {
     const auth = req.headers.authorization
