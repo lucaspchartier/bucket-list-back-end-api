1ec37a704802ecf1adee9882cd62148843ab1d65
lucaspchartier <lucaspchartier@gmail.com>
2020-11-23 14:15:08 -0500

    Uncomment res headers in app.use() function.

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ const app = express()
 
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
-// app.options('*', cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
+// app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
+app.options('*', cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
 
 // define port for API to run on
 const port = process.env.PORT || 4741
@@ -53,8 +53,8 @@ const port = process.env.PORT || 4741
 // of `Authorization: Token token=<token>` OR the Express convention of
 // `Authorization: Bearer <token>`
 app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', '*')
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   if (req.headers.authorization) {
     const auth = req.headers.authorization
     // if we find the Rails pattern in the header, replace it with the Express
